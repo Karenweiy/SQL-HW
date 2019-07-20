e7dbcdcc90726d8f8d96b6699b1af6e9842bce71
Karen Wei <karenweiy@gmail.com>
2019-07-20 16:00:50 -0400

    Revised version

---
+++ SQL_HW _ schemata.sql
@@ -0,0 +1,54 @@
+DROP TABLE IF EXISTS departments;
+DROP TABLE IF EXISTS dept_emp;
+DROP TABLE IF EXISTS dept_manager;
+DROP TABLE IF EXISTS employees;
+DROP TABLE IF EXISTS salaries;
+DROP TABLE IF EXISTS titles;
+
+create table departments(
+    dept_no VARCHAR(100) NOT NULL,
+    dept_name VARCHAR(100) NOT NULL,
+    primary key(dept_no)
+    );
+	
+create table dept_emp(
+    emp_no Int NOT NULL,
+    dept_no VARCHAR(1000000)NOT NULL,
+    from_date Date NOT NULL,
+    to_date Date NOT NULL
+    );
+--comment: have error when creating foreign key of emp_no and dept_no, so drop those quiries
+	
+create table dept_manager(
+    dept_no VARCHAR(1000000)NOT NULL,
+    emp_no Int NOT NULL,
+    from_date Date NOT NULL,
+    to_date Date NOT NULL
+    );
+--comment: have error when creating foreign key of emp_no and dept_no, so drop those quiries
+
+create table employees(
+    emp_no Int NOT NULL,
+    birth_date Date NOT NULL,
+    first_name VARCHAR(1000000)NOT NULL,
+    last_name VARCHAR(1000000)NOT NULL,
+    gender VARCHAR(1000000)NOT NULL,
+    hire_date Date NOT NULL
+    primary key(emp_no)
+    );
+
+create table salaries(
+    emp_no Int NOT NULL,
+    salary Int NOT NULL,
+    from_date Date NOT NULL,
+    to_date Date NOT NULL
+    );
+--comment: have error when creating foreign key of emp_no, so drop that quiry
+
+create table titles(
+    emp_no Int NOT NULL,
+    title VARCHAR(1000000),
+    from_date Date NOT NULL,
+    to_date Date NOT NULL
+    );  
+--comment: have error when creating foreign key of emp_no, so drop that quiry
--- SQL_HW_ quiries.sql
+++ SQL_HW_ quiries.sql
@@ -1,65 +1,64 @@
 --1. List the following details of each employee: employee number, last name, first name, gender, and salary. 
-create view emp_info as 
-select e.emp_no, e.last_name, e.first_name, e.gender,s.salary
-from employees as e, salaries as s ;
-select * from emp_info;
+select e.emp_no, e.last_name,e.first_name, e.gender,  s.salary
+from employees e 
+join salaries s
+on e.emp_no=s.emp_no;
 
 
 -- 2. List employees who were hired in 1986.
-
-create view hire_1986 as
 select first_name, last_name, hire_date
 from employees
 where EXTRACT(year FROM "hire_date") = 1986;
-select * from hire_1986;
 
 
 -- 3. List the manager of each department with the following information: department number, department name, the manager's employee number, last name, first name, and start and end employment dates.
 
-create view manager as
-select e.dept_no, e.dept_name, m.emp_no, m.last_name, m.first_name, m.from_date, m.to_date
-from employees as e, dept_manager as m;
-select * from manager;
+select d.dept_no, d.dept_name, dm.emp_no,e.last_name, e.first_name, dm.from_date, dm.to_date
+from departments d 
+join dept_manager dm
+on d.dept_no = dm.dept_no
+join employees e
+on dm.emp_no = e.emp_no;
 
 
 -- 4. List the department of each employee with the following information: employee number, last name, first name, and department name.
 
-create view department as
-select e.emp_no, e.last_name, e.first_name, d.dept_name
-from employees e, departments d;
-select * from department;
+select e.emp_no,e.last_name, e.first_name, d.dept_name
+from dep_emp ed
+join departments d
+on  ed.dept_no =d.dept_no
+join employees e
+on ed.emp_no = e.emp_no;
 
 
 -- 5. List all employees whose first name is "Hercules" and last names begin with "B."
 
-create view emp_Hercules as
-select last_name, first_name
-from employees where first_name = 'Hercules' and last_name LIKE 'B%';
-select * from emp_Hercules;
+select first_name, last_name from employees 
+where first_name = 'Hercules'
+and
+last_name like 'B%';
 
 
 -- 6. List all employees in the Sales department, including their employee number, last name, first name, and department name.
 
-select emp_no, last_name, first_name
-from employees where emp_no in
-(select emp_no
- from dept_emp where dept_no IN
-(select dept_no
-from departments where dept_name = 'Sales')
-);
+select e.emp_no,e.last_name, e.first_name,d.dept_name
+from employees e
+join dept_emp de
+on e.emp_no = de.emp_no
+join departments d
+on de.dept_no = d.dept_no 
+where d.dept_name = 'Sales';
 
 -- 7. List all employees in the Sales and Development departments, including their employee number, last name, first name, and department name.
 
-select e.emp_no, e.last_name, e.first_name, d.dept_name
-from employees as e, departments as d
-where emp_no in
-( select emp_no
-  from dept_emp
-  where dept_no in
-  ( select dept_no
-    from departments 
-    where dept_name = 'Sales' or dept_name = 'Development')
-);
+select e.emp_no,e.last_name, e.first_name,d.dept_name
+from employees e
+join dept_emp de
+on e.emp_no = de.emp_no
+join departments d
+on de.dept_no = d.dept_no 
+where d.dept_name = 'Sales'
+or d.dept_name = 'Development';
 
 
 -- 8. In descending order, list the frequency count of employee last names, i.e., how many employees share each last name.
@@ -67,4 +66,4 @@ where emp_no in
 select last_name, count(last_name)
 from employees
 group by last_name
-order by last_name DESC;
+order by count(last_name) DESC;
